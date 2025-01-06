167f9682a4dc0c39fa34c9e0b26914190c0badb3
msuryasyahruli <msuryasyahruli12@gmail.com>
2025-01-06 11:36:30 +0700

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Navigate, RouterProvider,createBrowserRouter } from 'react-router-dom'
-import Dasboard from './pages/Dasboard'
-import Request from './pages/Request'
-import 'bootstrap/dist/js/bootstrap.js'
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Request from "./pages/Request";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/css/bootstrap.css";
 
-const router = createBrowserRouter([
-  {path:"/",element:<Navigate to="/system-request/dasboard"/>},
-  {path:"/system-request/dasboard",element:<Dasboard/>},
-  {path:"/request/pickup-part-import",element:<Request/>}
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/system-request/dashboard" />} />
+        <Route path="/system-request/dashboard" element={<Dashboard />} />
+        <Route path="/request/pickup-part-import" element={<Request />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
